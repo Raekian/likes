649f69a0b2e161f65a1f8f3e70648b97b5c6d119
Raekian <Stevenzerwas@gmail.com>
2021-05-24 14:06:23 -0700

    init

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+var likesOne = 9;
+var likeElementOne = document.querySelector("#likesOne")
+
+function addLikeBoxOne( ) {
+    likesOne++;
+    likeElementOne.innerText = likesOne + " like(s)"
+    console.log(likesOne)
+}
+
+var likesTwo = 12;
+var likeElementTwo = document.querySelector("#likesTwo")
+
+function addLikeBoxTwo( ) {
+    likesTwo++;
+    likeElementTwo.innerText = likesTwo + " like(s)"
+    console.log(likesTwo)
+}
+
+var likesThree = 9;
+var likeElementThree = document.querySelector("#likesThree")
+
+function addLikeBoxThree( ) {
+    likesThree++;
+    likeElementThree.innerText = likesThree + " like(s)"
+    console.log(likesThree)
+}
